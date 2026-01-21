9f8cc5630bad694642f0af46abf93f1f3a560296
yunminjae03 <dbsaslwo012@g.hongik.ac.kr>
2026-01-21 16:48:21 +0900

    refactor : conflict 해결

--- src/main/java/com/meta/memo/controller/MemoController.java
+++ src/main/java/com/meta/memo/controller/MemoController.java
@@ -1,46 +1,29 @@
 package com.meta.memo.controller;
 
-
-import com.meta.memo.domain.Memo;
 import com.meta.memo.dto.MemoRequestDto;
 import com.meta.memo.dto.MemoResponseDto;
 import com.meta.memo.service.MemoService;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("api/memos")
-
 public class MemoController {
-    // 멤버 변수 선언
+
     private final MemoService memoService;
 
-    //생성자 주입(DI)
-    public MemoController(MemoService memoService){
-        this.memoService =  memoService;
+    public MemoController(MemoService memoService) {
+        this.memoService = memoService;
     }
 
-    @PostMapping()
+    @PostMapping
     public MemoResponseDto createMemo(@RequestBody MemoRequestDto memoRequestDto) {
         return memoService.createMemo(memoRequestDto);
     }
 
-    @GetMapping()
-    public List<MemoResponseDto> getMemos(){
+    @GetMapping
+    public List<MemoResponseDto> getMemos() {
         return memoService.getMemos();
     }
 
@@ -50,7 +33,7 @@ public class MemoController {
     }
 
     @DeleteMapping("{id}")
-    public Long deleteMemo(@PathVariable Long id){
+    public Long deleteMemo(@PathVariable Long id) {
         return memoService.deleteMemo(id);
     }
 }
