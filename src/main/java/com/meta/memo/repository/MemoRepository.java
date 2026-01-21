6515ed061c8dd89d69f55dae880b689fee6897d1
yunminjae03 <dbsaslwo012@g.hongik.ac.kr>
2026-01-21 11:53:06 +0900

    refactor : JPA가 스캔 할 수 있는 엔터티 클래스로 변경

--- src/main/java/com/meta/memo/repository/MemoRepository.java
+++ src/main/java/com/meta/memo/repository/MemoRepository.java
@@ -8,13 +8,14 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-@Component
+@Repository
 public class MemoRepository {
   // JDBC 멤버 변수 선언
   private final JdbcTemplate jdbcTemplate;
