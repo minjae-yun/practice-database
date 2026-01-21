302952ab1dfe7071fe0cc4e865404988eefafc09
yunminjae03 <dbsaslwo012@g.hongik.ac.kr>
2026-01-21 14:34:46 +0900

    refactor : Service 계층에서 트랜잭션 설정(CUD) / 읽기전용(R)

--- src/main/java/com/meta/memo/service/MemoService.java
+++ src/main/java/com/meta/memo/service/MemoService.java
@@ -4,15 +4,16 @@ import com.meta.memo.domain.Memo;
 import com.meta.memo.dto.MemoRequestDto;
 import com.meta.memo.dto.MemoResponseDto;
 import com.meta.memo.repository.MemoRepository;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+
 
 import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
 public class MemoService {
   // 멤버 변수 선언
   private final MemoRepository memoRepository;
@@ -21,11 +22,10 @@ public class MemoService {
   public MemoService(MemoRepository memoRepository) {
     this.memoRepository = memoRepository;
   }
-
+  @Transactional
   public MemoResponseDto createMemo(@RequestBody MemoRequestDto memoRequestDto) {
     // RequestDto -> Entity 변환
     Memo newMemo = new Memo(memoRequestDto);
-
     Memo savedMemo = memoRepository.save(newMemo);
 
     // Entity -> ResponseDto 변환
@@ -34,37 +34,37 @@ public class MemoService {
     return memoResponseDto;
   }
 
+
+  public Memo getMemoById(Long id){
+    return memoRepository.findById(id).orElseThrow(() ->
+        new IllegalArgumentException("선택한 id의 메모는 존재하지 않습니다."));
+  }
+
   public List<MemoResponseDto> getMemos() {
-    List<MemoResponseDto> memoResponseDtoList = memoRepository.findAll();
+    List<MemoResponseDto> memoResponseDtoList = memoRepository.findAll().stream()
+        .map(MemoResponseDto::new).toList();
     return memoResponseDtoList;
   }
 
+  @Transactional
   public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto memoRequestDto) {
 
     // 해당 id의 메모가 존재하는지 확인
-    Memo foundMemo = memoRepository.findById(id);
+    Memo foundMemo = getMemoById(id);
 
     // 메모 내용 수정
-    if (foundMemo != null) {
-      Long updatedId = memoRepository.update(id, memoRequestDto);
-      return updatedId;
-    } else {
-      throw new IllegalArgumentException("선택한 id의 메모는 존재하지 않습니다.");
-    }
+    foundMemo.update(memoRequestDto);
+    return id;
   }
-
+  @Transactional
   public Long deleteMemo(@PathVariable Long id) {
 
     // 해당 id의 메모가 존재하는지 확인
-    Memo foundMemo = memoRepository.findById(id);
-
+    Memo foundMemo = getMemoById(id);
     // 메모 내용 삭제
-    if (foundMemo != null) {
-      Long deletedId = memoRepository.delete(id);
-      return deletedId;
-    } else {
-      throw new IllegalArgumentException("선택한 id의 메모는 존재하지 않습니다.");
-    }
+    memoRepository.delete(foundMemo);
+    return id;
+
   }
 
 }
